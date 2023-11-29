8e19732b7e282d27cc4f244e12fc997b5cb954bc
Yash Dubey <52771855+yashdubey132@users.noreply.github.com>
2023-11-29 07:38:03 +0100

    Update main.py
    Removed erroneous file path

--- main.py
+++ main.py
@@ -25,11 +25,11 @@ MONTH_DAYS_MID = [16, 46, 75, 106, 136, 167, 197, 228, 259, 289, 320, 350] # day
 TIME_SHIFT_TO_UTC = +1 # UTC to MST
 
 #CSV_PATH_TEMPERATURE = 'TempZurichHourly.csv'
-CSV_PATH_TEMPERATURE = 'TempSwitzerland.csv'
+#CSV_PATH_TEMPERATURE = 'TempSwitzerland.csv'
 CSV_PATH_TEMPERATURE = "/Users/yash/Documents/GitHub/temp_trim/TempSwitzerland.csv"
 #CSV_PATH_SOLAR_IRRADIATION = 'SRZurichHourly.csv'
 CSV_PATH_SOLAR_IRRADIATION = 'SRBSwitzerland.csv'
-CSV_PATH_SOLAR_IRRADIATION = "/Users/yash/Documents/GitHub/temp_trim/SRBSwitzerland.csv"
+#CSV_PATH_SOLAR_IRRADIATION = "/Users/yash/Documents/GitHub/temp_trim/SRBSwitzerland.csv"
 
 
 
